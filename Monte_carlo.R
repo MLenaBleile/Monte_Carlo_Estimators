c7ed230bb71b5d5fd9b8b2b557d8bb3dcef52d03
Makanjuola A Ogunleye <48253625+makanju0la@users.noreply.github.com>
2019-11-05 13:34:43 -0600

    Update Monte_carlo.R

--- Monte_carlo.R
+++ Monte_carlo.R
@@ -5,3 +5,30 @@ rtrim <- function(x, r){
   est <- sum(sort(x)[(n*r + 1) : (n - n*r)])/(n - 2*(n*r))
   return(est)
 }
+
+#r- winsorized mean
+rwinsor<- function(x, r){
+  n = length(x)
+  x = sort(x)
+  est <- (sum(x[(n*r + 2) : (n - n*r -1)]) + (n*r+1) * x[n*r + 1] + (n*r+1) * x[n - n*r] )/n
+  return(est)
+}
+
+#r - linearly weighted mean
+rlin <- function(x, r){
+  n = length(x)
+  x = sort(x)
+  vec <- seq(1, (n/2 - n*r))
+  total = 0
+  for (i in 1:length(vec)){
+    y = (2*i - 1) * (x[n*r+i] + x[n-n*r-i+1])
+    total = total + y
+  }
+  est = (1/(2*(n/2 - n*r)^2)) * total
+  return(est)
+}
+
+#test cases
+xx<- c(12,14,23,16,24,30,1,2,4,5)
+rwinsor(xx,0.0) == mean(xx)
+rtrim(xx,0.0) == mean(xx)
