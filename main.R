5ff5ac402567aae6fa35a1c7ad8b868b10b9a43f
Mary Lena Bleile <mbleile@smu.edu>
2019-11-09 15:08:43 -0600

    Update main.R

--- main.R
+++ main.R
@@ -1,7 +1,3 @@
-setwd("C:/Users/MaryLena/Desktop/FALL2019/STAT 6324/MonteCarlo")
-source("gen_estimators.R")
-
-
 estimate = c("Mean", "Median", "Trim1", "Trim2", "Wind1", "Wind2", "Lin1", "Lin2")
 n_char = c(10,20,30,50)
 estim_names = unlist(outer(estimate, n_char, paste))
@@ -16,10 +12,12 @@ d_FO2 = M_estimates(M, F_O_2, estim_names)
 d_Lap = M_estimates(M, laplace_es, estim_names)
 d_Log = M_estimates(M, Logistic_e, estim_names)
 
-get_rank_estimates(d_norm)
-get_rank_estimates(d_RO)
-get_rank_estimates(d_RO2)
-get_rank_estimates(d_FO1)
-get_rank_estimates(d_FO2)
-get_rank_estimates(d_Lap)
-get_rank_estimates(d_Log)
+
+Normal = best_estimates(get_estimates(d_norm))
+Random_Outlier_1 = best_estimates(get_estimates(d_RO))
+Random_Outlier_2 = best_estimates(get_estimates(d_RO2))
+Fixed_Outlier_1 = best_estimates(get_estimates(d_FO1))
+Fixed_Outlier_2 = best_estimates(get_estimates(d_FO2))
+Laplace = best_estimates(get_estimates(d_Lap))
+Logarithmic = best_estimates(get_estimates(d_Log))
+
